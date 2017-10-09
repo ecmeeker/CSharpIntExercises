12b1e4f5e291ddbf64fe780d54b5ed2e2a46881a
Emily Cassandra Meeker <ecmeeker@users.noreply.github.com>
2017-10-09 17:48:09 -0500

    Add files via upload
    Add project files for SOPost

---
+++ SOPost/SOPost/SOPost.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOPost
+{
+  class SOPost
+  {
+    private string _title;
+    private string _description;
+    private DateTime _created;
+    private int _voteCount;
+
+    public SOPost(string title, string description)
+    {
+      Title = title;
+      Description = description;
+      Created = DateTime.Now;
+      VoteCount = 0;
+    }
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Created { get; private set; }
+    public int VoteCount { get; private set; }
+
+    public void UpVote()
+    {
+      VoteCount += 1;
+    }
+
+    public void DownVote()
+    {
+      VoteCount -= 1;
+    }
+  }
+}
