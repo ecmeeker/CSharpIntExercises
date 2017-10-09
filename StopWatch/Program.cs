3c918f8007d35d0320f2231c7e9d4e74d9ab4ae9
Emily Cassandra Meeker <ecmeeker@users.noreply.github.com>
2017-10-09 12:03:38 -0500

    Add files via upload
    Adding proj files

---
+++ StopWatch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Creating Stopwatch:");
+      Stopwatch watch = new Stopwatch();
+
+      watch.Start();
+      System.Threading.Thread.Sleep(8000);
+      Console.WriteLine(watch.Stop());
+      watch.Reset();
+
+      watch.Start();
+      System.Threading.Thread.Sleep(4000);
+      Console.WriteLine(watch.Stop());
+      watch.Start();
+      System.Threading.Thread.Sleep(5000);
+      Console.WriteLine(watch.Stop());
+      watch.Start();
+      System.Threading.Thread.Sleep(3000);
+      Console.WriteLine(watch.Stop());
+
+      watch.Start();
+      System.Threading.Thread.Sleep(2000);
+      watch.Start();
+      System.Threading.Thread.Sleep(1000);
+      Console.WriteLine(watch.Stop());
+      watch.Reset();
+
+      System.Threading.Thread.Sleep(3000);
+
+    }
+  }
+}
---
+++ StopWatch/Stopwatch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+  class Stopwatch
+  {
+    private DateTime _startTime;
+    private DateTime _stopTime;
+
+    public Stopwatch() { }
+
+    public DateTime StartTime { get; set; }
+    public DateTime StopTime { get; set; }
+    public TimeSpan Duration
+    {
+      get
+      {
+        return StopTime - StartTime;
+      }
+    }
+    public void Start()
+    {
+      if (StartTime > StopTime)
+        throw new InvalidOperationException("Timer has already been started!");
+      else
+        StartTime = DateTime.Now;
+      return;
+    }
+    public TimeSpan Stop()
+    {
+      StopTime = DateTime.Now;
+      return Duration;
+    }
+    public void Reset()
+    {
+      StartTime = DateTime.MinValue;
+      StopTime = DateTime.MinValue;
+      return;
+    }
+
+  }
+}
