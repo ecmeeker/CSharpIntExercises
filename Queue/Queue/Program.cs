6f854b2e01858ac5354d924b4dc19d8d4f11168c
Emily Cassandra Meeker <ecmeeker@users.noreply.github.com>
2018-10-31 21:48:54 -0500

    Added Queue data structure exercise

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<int>();
+
+            queue.Enqueue(3);
+            queue.Enqueue(6);
+            queue.Enqueue(9);
+
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+
+            queue.Clear();
+
+            Console.ReadKey();
+
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Queue/Queue/Queue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueImplementation
+{
+    class Queue
+    {
+        private LinkedList<object> _queue;
+
+        public Queue()
+        {
+            _queue = new LinkedList<object>();
+        }
+
+        public void Enqueue(object obj)
+        {
+            if (_queue != null)
+            {
+                _queue.AddLast(obj);
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot add to a queue until it exists!");
+            }            
+        }
+
+        public object Dequeue()
+        {
+            object first = null;
+            if (_queue != null && _queue.Count > 0)
+            {
+                first = Peek();
+                _queue.RemoveFirst();
+            }
+            else if (_queue != null)
+            {
+                throw new InvalidOperationException("Queue is empty!");
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot access a nonexistent queue!");
+            }
+            return first;
+        }
+
+        public object Peek()
+        {
+            object first = null;
+            if (_queue != null && _queue.Count > 0)
+            {
+                first = _queue.First();
+            }
+            else if (_queue != null)
+            {
+                throw new InvalidOperationException("Queue is empty!");
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot access a nonexistent queue!");
+            }
+            return first;
+        }
+
+        public void Clear()
+        {
+            if (_queue != null)
+            {
+                _queue.Clear();
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot access a queue until it exists!");
+            }
+        }
+    }
+}
